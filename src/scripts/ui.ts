a72560d3379a22360553452f65ba10c5daacd338
Erez Ben Moshe <erezbmp@gmail.com>
2021-02-06 19:19:30 +0200

    Add slider

--- src/scripts/ui.ts
+++ src/scripts/ui.ts
@@ -1,6 +1,7 @@
 import { MDCTopAppBar } from '@material/top-app-bar';
 import { MDCRipple } from '@material/ripple';
 import { MDCDrawer } from '@material/drawer';
+import { MDCSlider } from '@material/slider';
 
 MDCTopAppBar.attachTo(document.querySelector('.mdc-top-app-bar')!);
 const settingsButton = document.getElementById('boids-settings-btn') as HTMLButtonElement;
@@ -8,8 +9,13 @@ MDCRipple.attachTo(settingsButton);
 const settingsDrawer = MDCDrawer.attachTo(document.getElementById('boids-settings-drawer')!);
 settingsDrawer.list?.listElements.forEach((listElement) => MDCRipple.attachTo(listElement));
 
+const numberOfBoidsSlider = new MDCSlider(document.getElementById('number-of-boids-slider')!);
+
 settingsButton.addEventListener('click', () => {
   settingsDrawer.open = !settingsDrawer.open;
+});
+document.body.addEventListener('MDCDrawer:opened', () => {
+  numberOfBoidsSlider.layout();
 });
 
 const canvas = document.getElementById('boids-canvas') as HTMLCanvasElement;
