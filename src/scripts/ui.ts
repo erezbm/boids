e73b36fdfcef20be3432348f528263c95308c743
Erez Ben Moshe <erezbmp@gmail.com>
2021-02-11 02:05:30 +0200

    Add a synchronized text field next to the slider

--- src/scripts/ui.ts
+++ src/scripts/ui.ts
@@ -2,14 +2,26 @@ import { MDCTopAppBar } from '@material/top-app-bar';
 import { MDCRipple } from '@material/ripple';
 import { MDCDrawer } from '@material/drawer';
 import { MDCSlider } from '@material/slider';
+import { MDCTextField } from '@material/textfield';
 
 MDCTopAppBar.attachTo(document.querySelector('.mdc-top-app-bar')!);
 const settingsButton = document.getElementById('boids-settings-btn') as HTMLButtonElement;
 MDCRipple.attachTo(settingsButton);
 const settingsDrawer = MDCDrawer.attachTo(document.getElementById('boids-settings-drawer')!);
 settingsDrawer.list?.listElements.forEach((listElement) => MDCRipple.attachTo(listElement));
+const numberOfBoidsTextField = new MDCTextField(document.getElementById('number-of-boids-text-field')!);
 
 const numberOfBoidsSlider = new MDCSlider(document.getElementById('number-of-boids-slider')!);
+
+numberOfBoidsSlider.listen('MDCSlider:input', () => {
+  numberOfBoidsTextField.value = numberOfBoidsSlider.getValue().toString();
+});
+
+numberOfBoidsTextField.listen('input', () => {
+  if (numberOfBoidsTextField.valid) {
+    numberOfBoidsSlider.setValue(Number(numberOfBoidsTextField.value));
+  }
+});
 
 settingsButton.addEventListener('click', () => {
   settingsDrawer.open = !settingsDrawer.open;
