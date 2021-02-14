1d91bed6b8050cf70041c6d14f8bfb01bbe5fd5c
erezbm <erezbenm@gmail.com>
2021-02-14 00:41:03 +0000

    Deploying to gh-pages from  @ 22238259870f7a73c86562ea5ec776de484e9e27 🚀

--- js/flags.js
+++
@@ -1,4 +0,0 @@
-export default {
-    debug: false,
-    image: new Image(),
-};
--- js/utils.js
+++
@@ -1,3 +0,0 @@
-export const clamp = (value, min, max) => Math.min(Math.max(value, min), max);
-export const mapRange = (value, x1, y1, x2, y2) => (value - x1) * ((y2 - x2) / (y1 - x1)) + x2;
-export const toRadians = (degrees) => degrees * (Math.PI / 180);
